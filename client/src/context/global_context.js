59e4bc34616cd46f1f1c70588e751a566b38f1e6
Stefan Benincasa <stefan.benincasa@protonmail.com>
2023-11-30 15:10:59 +1100

    minor change to initial global state;

--- client/src/context/global_context.js
+++ client/src/context/global_context.js
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useState } from 'react';
 export const GlobalContext = createContext()
 
 export function GlobalProvider({ children }) {
-  const [globalContext, setGlobalContext] = useState({})
+  const [globalContext, setGlobalContext] = useState({session: {}})
 
   return (
     <GlobalContext.Provider value={{globalContext, setGlobalContext}}>
