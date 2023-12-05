bf2bc9eced8de26c034f3744f0213fb1fe328a4c
Stefan Benincasa <stefan.benincasa@protonmail.com>
2023-12-05 15:50:54 +1100

    feature for additionalClasses to Loader;

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react"
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import { isAuthenticated } from '../services/secure';
 
-function Loader({info = "", variant = "primary"}) {
+function Loader({info = "", variant = "primary", additionalClasses = ""}) {
     return (
         <div className="d-flex flex-column justify-content-center align-items-center">
             <strong className={"text-center text-" + variant}>{info}</strong>
