673ccd0e51c86eae2f0f6fc7505bc9a28289065f
Stefan Benincasa <stefan.benincasa@protonmail.com>
2023-12-05 15:18:02 +1100

    handle null arguments for Loader;

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react"
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import { isAuthenticated } from '../services/secure';
 
-function Loader({info, variant}) {
+function Loader({info = "", variant = "primary"}) {
     return (
         <div className="d-flex flex-column justify-content-center align-items-center">
-            <strong className={"text-center text-" + (variant ? variant : "primary")}>{info}</strong>
-            <div className={"spinner-grow text-" + (variant ? variant : "primary")} role="status" style={{ width: "5rem", height: "5rem" }}></div>
+            <strong className={"text-center text-" + variant}>{info}</strong>
+            <div className={"spinner-grow text-" + variant} role="status" style={{ width: "5rem", height: "5rem" }}></div>
         </div>
     )
 }
