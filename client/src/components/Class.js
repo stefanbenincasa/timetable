c1afb8dccc2e7f8707c05e65869416a2a8b404fc
Stefan Benincasa <stefan.benincasa@protonmail.com>
2023-12-06 16:35:21 +1100

    new class component;

---
+++ client/src/components/Class.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react"
+import { Link, Navigate, useNavigate } from "react-router-dom";
+
+function Class() {
+    return (
+        <div className="Class d-flex flex-column justify-content-center align-items-center">
+            This is a Class item
+        </div>
+    )
+}
+
+export default Class
