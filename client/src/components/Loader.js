48d70b9b7d590ffafdf1ab1c202fa7cec3eba605
Stefan Benincasa <stefan.benincasa@protonmail.com>
2023-12-06 16:35:07 +1100

    minor changes;

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react"
 import { Link, Navigate, useNavigate } from "react-router-dom";
-import { isAuthenticated } from '../services/secure';
 
 function Loader({info = "", variant = "primary", additionalClasses = ""}) {
     return (
-        <div className="d-flex flex-column justify-content-center align-items-center">
+        <div className="Loader d-flex flex-column justify-content-center align-items-center">
             <strong className={"text-center text-" + variant}>{info}</strong>
             <div className={"spinner-grow text-" + variant} role="status" style={{ width: "5rem", height: "5rem" }}></div>
         </div>
