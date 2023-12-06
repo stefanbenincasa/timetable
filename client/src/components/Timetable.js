b78dbde90fe79d11c3614cf50d6a26bb1c9216eb
Stefan Benincasa <stefan.benincasa@protonmail.com>
2023-12-06 17:36:12 +1100

    work on Timetable logic;

--- client/src/components/Timetable.js
+++ client/src/components/Timetable.js
@@ -1,34 +1,52 @@
 import React, { useState, useEffect } from 'react';
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Link, Navigate, json, useNavigate } from "react-router-dom";
 
-const initialState = { 
-  studentId: 1, 
-  classes: [
-      {
-          classId: 1,
-          teacher: "Dr Henry Morgan",
-          dateTime: "2023-06-15T03:30:00.000Z",
-          durationMinutes: 60
-      },
-      {
-          classId: 2,
-          teacher: "Dr Simon Watson",
-          dateTime: "2023-06-15T03:30:00.000Z",
-          durationMinutes: 120
+import Class from "./Class"
+import Loader from "./Loader"
+
+/* const initialState = [
+  {
+      classId: 1,
+      teacher: "Dr Henry Morgan",
+      dateTime: "2023-06-15T03:30:00.000Z",
+      durationMinutes: 60
+  },
+  {
+      classId: 2,
+      teacher: "Dr Simon Watson",
+      dateTime: "2023-06-15T03:30:00.000Z",
+      durationMinutes: 120
+  }
+]
+ */
+
+function Timetable({handleLogout}) {
+  const [ classes, setClasses ] = useState(null)
+
+  useEffect(() => {
+    async function getTimetable() {
+      const response = await fetch(`http://localhost:5000/timetable`, { credentials: "include" })
+      if(response.status == 401) {
+        handleLogout()
+        return
       }
-  ]
-}
 
-function Timetable() {
-  const [ timetable, setTimetable ] = useState(initialState)
+      let { studentId, classes } = await response.json()
+      setClasses(classes ? classes : null)
+    }
 
-  const navigate = useNavigate()
-
+    setTimeout(() => getTimetable(), 1500)
+  }, [setClasses])
+  
+ /*Remember to handle the NULL case; consider filler element to notify User*/
   return (
     <div id="Timetable" className="w-100 p-5 row rounded border">
         <header className="h-25 col-12 d-flex justify-content-center align-items-center rounded bg-primary text-white"><h2>Timetable</h2></header>
-        <div className="h-75 p-0 col-12"> 
-          { timetable.classes.map((currClass, index) => { return <p key={index}>{currClass.teacher}</p> }) }
+        <div className={"h-75 p-0 col-12 d-flex flex-column" + (!classes ? " justify-content-center align-items-center" : "")}> 
+          { 
+            classes === null && <Loader /> ||
+            classes.map((currClass, index) => { return <p key={index}>{currClass.teacher}</p> })
+          }
         </div>
     </div> 
   )
