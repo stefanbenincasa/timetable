d77ab858904569ffeae4316cdb01b6759c6ab198
Stefan Benincasa <stefan.benincasa@protonmail.com>
2023-12-06 17:35:23 +1100

    refine of Loader;

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -3,7 +3,7 @@ import { Link, Navigate, useNavigate } from "react-router-dom";
 
 function Loader({info = "", variant = "primary", additionalClasses = ""}) {
     return (
-        <div className="Loader d-flex flex-column justify-content-center align-items-center">
+        <div className={"Loader d-flex flex-column justify-content-center align-items-center" + " " + additionalClasses}>
             <strong className={"text-center text-" + variant}>{info}</strong>
             <div className={"spinner-grow text-" + variant} role="status" style={{ width: "5rem", height: "5rem" }}></div>
         </div>
