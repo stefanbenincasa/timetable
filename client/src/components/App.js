4d70a662623351f3420c369f62eea9a75cba97d3
Stefan Benincasa <stefan.benincasa@protonmail.com>
2023-12-12 21:50:37 +1100

    finalise Timetable;

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,8 +7,6 @@ import Timetable from "./Timetable"
 import Loader from "./Loader";
 import Login from "./Login"
 
-// import { ReactComponent as LogoutIcon } from "../images/log-out-outline.svg"
-
 import '../styles/App.css';
 
 function App() {
@@ -39,7 +37,7 @@ function App() {
   }
 
   return (
-  <div id="App" className="h-100 p-5 m-auto container-fluid d-flex flex-column align-items-center justify-content-center">
+  <div id="App" className="p-5 m-auto container-fluid d-flex flex-column align-items-center justify-content-center">
       { 
         isLoggedIn && 
         <nav className="p-0 d-flex justify-content-center align-items-center rounded bg-primary text-white" onClick={handleLogout}>
@@ -54,7 +52,6 @@ function App() {
         <Route path="/" element={isLoggedIn === true ? <Timetable handleLogout={handleLogout} /> : <Navigate to="/login" /> } />
       </Routes>
 
-      <aside className="text-white">Background Image Photo: by <a className="text-white" target="_blank" href="https://unsplash.com/@timmossholder?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Tim Mossholder</a></aside>
     </div>
   )
 }
--- client/src/components/Timetable.js
+++ client/src/components/Timetable.js
@@ -3,39 +3,59 @@ import { Link, Navigate, useNavigate } from "react-router-dom";
 
 import Class from "./Class"
 import Loader from "./Loader"
+import NoResults from "./NoResults"
 
 function Timetable({handleLogout}) {
   const [ classes, setClasses ] = useState(null)
-  const [ additionalClasses, setAdditionalClasses] = useState("")
+  const [ additionalClasses, setAdditionalClasses ] = useState("")
+  const [ isWithinLoadTime, setIsWithinLoadTime ] = useState(true)
 
   useEffect(() => {
     async function getTimetable() {
-      const response = await fetch(`http://localhost:5000/timetable`, { credentials: "include" })
-      if(response.status == 401) {
-        handleLogout()
-        return
-      }
 
-      let { studentId, classes } = await response.json()
-      setClasses(classes ? classes : null)
+      try {
+        let timeMsSinceFetch = 0, maxResponseWaitTime = 8500, msIncrement = 2500, intervalId = null
+
+        const startResponseCountdown = function() {
+          intervalId = setInterval(() => {
+            timeMsSinceFetch += msIncrement
+            if(timeMsSinceFetch >= maxResponseWaitTime) {
+              setIsWithinLoadTime(false)
+              clearInterval(intervalId)
+            }
+          }, 2500)
+        }
+
+        startResponseCountdown()
+        const response = await fetch(`http://localhost:5000/timetable`, { credentials: "include" })
+        
+        if(response.status == 401) {
+          handleLogout()
+          return
+        }
+
+        if(isWithinLoadTime) {
+          let { studentId, classes } = await response.json()
+          clearInterval(intervalId)
+          setClasses(classes ? classes : null)
+        }
+      }
+      catch(err) {
+        console.log(err)
+      }
     }
 
     setTimeout(() => getTimetable(), 1500)
   }, [setClasses])
 
-  const displayScrollBar = function(e) {
-  }
-  
-  const hideScrollBar = function(e) {
-  }
-  
- /*Remember to handle the NULL case; consider filler element to notify User*/
   return (
-    <div id="Timetable" className="w-100 p-4 row rounded border border-5 border-white">
-        <header className="p-5 col-12 d-flex justify-content-center align-items-center rounded bg-primary text-white"><h2>Timetable</h2></header>
-        <div className={"h-75 w-100 p-0 col-6 d-flex flex-column" + " " + additionalClasses + (!classes ? " justify-content-center align-items-center" : "")} onMouseEnter={displayScrollBar} onMouseOut={hideScrollBar}> 
+    <div id="Timetable" className="w-100 my-5 p-4 rounded border border-5 border-white">
+        <header className="p-5 d-flex justify-content-center align-items-center rounded bg-primary text-white"><h2>Timetable</h2></header>
+        <div className={"w-100 m-auto d-flex flex-column" + " " + additionalClasses + (!classes ? " justify-content-center align-items-center" : "")}> 
           { 
+            !isWithinLoadTime && <NoResults /> ||
             classes === null && <Loader /> ||
+            classes.length === 0 && <NoResults /> ||
             classes.map((cls, index) => { 
               return <Class key={index} classId={cls.classId} 
               teacher={cls.teacher} dateTime={cls.dateTime} 
